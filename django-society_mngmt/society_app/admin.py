e26483fe88c337fdb13ac2b6a18bdb67eca24257
MadyWeborPydjango <64833831+MadyWeborPydjango@users.noreply.github.com>
2020-07-07 08:44:12 +0530

    Add files via upload

---
+++ django-society_mngmt/society_app/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from society_app.models import *
+# Register your models here.
+admin.site.register(SocietyAllCharges)
+admin.site.register(RegSocietyMember)
+admin.site.register(MembersCharges)
+admin.site.register(MembersBills)
---
+++ django-society_mngmt/society_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class SocietyAppConfig(AppConfig):
+    name = 'society_app'
